c3957759f94095ec94e765f990337aad61c1786c
daryl-goh <goh.daryl@gmail.com>
2023-01-25 13:54:57 +0800

    NgStyle

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -3,21 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent {
-
-  title = "Employee page"
+  title = 'Employee page';
 
   number1 = 12;
   number2 = 20;
   // visible = (this.number1 > this.number2) ? true : false;
-  visible = this.ShowContent(this.number1,this.number2);
+  visible = this.ShowContent(this.number1, this.number2);
 
   IsVisible = true;
-Name: any;
+  Name: any;
 
-  ShowContent(val1: number, val2: number) : boolean {
+  ShowContent(val1: number, val2: number): boolean {
     if (val1 < val2) {
       return true;
     } else {
@@ -25,37 +24,36 @@ Name: any;
     }
   }
 
-  OnChange(arg: any){
+  OnChange(arg: any) {
     this.IsVisible = arg;
-
   }
-  
+
   color: any;
 
   SetColor(event: any) {
-    this.color= event.target.value;
-    }
+    this.color = event.target.value;
+  }
 
-  Employee : any[] = [];
+  Employee: any[] = [];
 
   constructor() {
     this.Employee = [
       {
         Name: 'Ah Kow',
         Age: '21',
-        Gender: 'M'
-
-      }, {
+        Gender: 'M',
+      },
+      {
         Name: 'Ah Meow',
         Age: '22',
-        Gender: 'F'
-      }, {
+        Gender: 'F',
+      },
+      {
         Name: 'Ah Tur',
         Age: '52',
-        Gender: 'M'
-      }
-    ]
-
+        Gender: 'M',
+      },
+    ];
   }
 
   GetMoreEmployee(): void {
@@ -63,17 +61,22 @@ Name: any;
       {
         Name: 'Amelia',
         Age: '11',
-        Gender: 'M'
-
-      }, {
+        Gender: 'M',
+      },
+      {
         Name: 'Benedict',
         Age: '27',
-        Gender: 'F'
-      }, {
+        Gender: 'F',
+      },
+      {
         Name: 'Catboy',
         Age: '2',
-        Gender: 'M'
-      }
-    ]
+        Gender: 'M',
+      },
+    ];
+  }
+
+  TrackByEmployeeName(index: number, employee: any): String {
+    return employee.Name;
   }
 }
